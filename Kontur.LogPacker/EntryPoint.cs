bee30e09810e792755a644d6c79b5c028b78b237
Vd0vin <furgit@mail.ru>
2019-05-18 03:50:10 +0500

    Add files via upload

---
+++ Kontur.LogPacker/EntryPoint.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+namespace Kontur.LogPacker
+{
+    internal static class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                var (inputFile, outputFile) = (args[0], args[1]);
+
+                if (File.Exists(inputFile))
+                {
+                    Compress(inputFile, outputFile);
+                    return;
+                }
+            }
+
+            if (args.Length == 3 && args[0] == "-d")
+            {
+                var (inputFile, outputFile) = (args[1], args[2]);
+
+                if (File.Exists(inputFile))
+                {
+                    Decompress(inputFile, outputFile);
+                    return;
+                }
+            }
+        }
+
+        private static void Compress(string inputFile, string outputFile)
+        {
+            var tempOutputFile = "temp";
+            Preparator.Preparator.PrepareForCompress(inputFile, tempOutputFile);
+            using (var inputStream = File.OpenRead(tempOutputFile))
+            using (var outputStream = File.OpenWrite(outputFile))
+            {
+                new GZipCompressor().Compress(inputStream, outputStream);
+            }
+        }
+
+        private static void Decompress(string inputFile, string outputFile)
+        {
+            var tempOutputFile = "temp";
+            using (var inputStream = File.OpenRead(inputFile))
+            using (var outputStream = File.OpenWrite(tempOutputFile))
+            {
+                new GZipCompressor().Decompress(inputStream, outputStream);
+            }
+            Preparator.Preparator.PrepareAfterDecompress(tempOutputFile, outputFile);
+        }  
+    }
+}
---
+++ Kontur.LogPacker/GZipCompressor.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Kontur.LogPacker
+{
+    internal class GZipCompressor
+    {
+        public void Compress(Stream inputStream, Stream outputStream)
+        {
+            using (var gzipStream = new BrotliStream(outputStream, CompressionLevel.Fastest, true)) 
+                inputStream.CopyTo(gzipStream);
+        }
+
+        public void Decompress(Stream inputStream, Stream outputStream)
+        {
+            using (var gzipStream = new BrotliStream(inputStream, CompressionMode.Decompress, true))
+                gzipStream.CopyTo(outputStream);
+        }
+    }
+}
